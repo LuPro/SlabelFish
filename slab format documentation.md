1e1770515b656f1f200b0308d80d404a7d744fc2
LuPro <luis.immanuel@gmx.at>
2021-08-29 07:08:11 +0200

    initial commit of minimum viable product for slab decoder

---
+++ slab format documentation.md
@@ -0,0 +1,35 @@
+# Slab data packing
+Slab data is first archived with gzip and the resulting binary data encoded with base64. To decode slabs you have to do these steps in reverse, so first decode with base64 and then unzip using gzip (in my test I just used the Python gzip library, Baggers has said for the last format the default C# gzip implementation works, ... It should be fairly straightforward)
+Once this is decoded you should be left with a string of binary data.
+
+# Binary Data Format
+*Disclaimer: Some variables seem to be little endian, some big endian, not 100% sure on that stuff*
+
+## Header [10 bytes]
+bytes 0-4 are always identical: 0x CE FA CE D1 02 (version identifier?)
+byte 5: Padding (= 0) (?)
+bytes 6-9: Number of unique assets in slab (Investigate, could also be bytes 5-6)
+
+## Asset list [n * 20 bytes]
+*Contains a list of all assets (tiles and props) in use in this slab*
+
+### Asset [20 bytes]
+bytes 0-15: UUID. Some "groups" seem to be reversed: `08:d2:cf:32:63:c3:34:44:b8:17:8b:a5:9f:ae:ed:17` -> `08:d2:cf:32` = reversed; `63:c3` = reversed; `34:44` = reversed; `b8:17` = not reversed; `8b:a5:9f:ae:ed:17` = not reversed. Actual resulting UUID is `32CFD208-C363-4434-B817-8BA59FAEED17`
+bytes 16-19: Number of instances of this tile
+
+next asset ... (is a list, no padding in between elements)
+
+## Asset positioning list [n * 8 bytes]
+*Contains a list of all asset positions (and rotations)*
+
+The asset positioning list contains each position of each asset, where one position consists of a "2 byte" (due to copy size limitations of 100x100x100, only 14 bits are ever used) x, y and z position and a "1 byte" (5 bits used) rotation. The asset positions contain no reference to the asset list, but they are ordered the same way. So if the asset list has on the first index 3 instances of the "Tavern Floor 2x2", the first 3 entries in the asset positioning list will be the 3 tavern floors and so on.
+
+### Asset position [8 bytes]
+When read as *little endian* the data is structured as follows *written in bits, not bytes*, where spaces are byte boundaries, "x", "y" and "z" mean bits that belong to the variable for their respective axis, "R" mean bits belonging to the rotation that are actually used, "r" are the "leftover" bits in the byte and "P" means padding: 
+`PPrrrRRR RRPPyyyy yyyyyyyy yyyyPPzz zzzzzzzz zzzzzzPP xxxxxxxx xxxxxxxx`
+
+The x, y and z coordinates are stored as integers where 100 means "1 tile" distance (1 grid unit). This means the slab format has 1/100 of a tile precision for prop placement.
+The rotation is stored as an integer from 0-23, where each value is a rotation "ID". The actual angle is calculated like this: `rot * 15`. Each rotation step is 15°, full 360° rotation are 24 steps (0-23)
+
+## End
+The end is 4 bytes padding.
---
+++ slab_decoder.py
@@ -0,0 +1,114 @@
+import base64
+import gzip
+import io
+import uuid
+from struct import *
+
+assets = {
+    "unique_asset_count": 0,
+    "asset_data": []
+}
+
+assetListEntryLength = 20
+assetPositionEntryLength = 8
+
+def assembleBytes(byteList):
+    total = 0
+    for i in range(len(byteList)):
+        total = (total << 8) + byteList[i] #take old total shift it to the left by 8 (make space for another byte), add next byte in list
+    return total
+
+def decodeAsset(assetData):
+    #print("\ndecoding raw asset", ':'.join(format(x, '02X') for x in tileData))
+    decData = unpack('IHH8BI', assetData)
+    uuid = (decData[0], decData[1], decData[2], assembleBytes(decData[3:5]), assembleBytes(decData[5:11]))
+    instanceCount = decData[11]
+    #print("\ndecoded tile data:", '-'.join(format(x, 'X') for x in decData))
+    #print("ordered decoded tile data:", '-'.join(format(x, 'X') for x in uuid))
+    uuidString = '-'.join(format(x, 'X') for x in uuid)
+    print("parsed UUID:", uuidString, "--- nr instances:", instanceCount)
+    assets["asset_data"].append({
+        "uuid": uuidString,
+        "instance_count": instanceCount,
+        "instances": [] #rename to instance_position? or just positions?
+    })
+
+def decodeAssetPosition(assetPositionData, decAssetCount):
+    #decode position from blob passed on
+    print("decoding raw asset position", ':'.join(format(x, '02X') for x in assetPositionData))
+    positionBlob = unpack("<Q", assetPositionData)[0]  #unpack as little endian. data is stored as 2 bit pad, 8 bit rot, 2 bit pad, 16 bit y, 2 bit pad, 16 bit z, 2 bit pad, 16 bit x
+    print("blob:", format(positionBlob, '064b'))
+    #x, y and z are 16bit, rot is 8 bit. all have 2 bit padding between each other.
+    x = positionBlob & 0xFFFF # isolate lowest 16 bits (x position)
+    y = (positionBlob >> 36) & 0xFFFF # shift away the x and z coord + 2*2 bit padding
+    z = (positionBlob >> 18) & 0xFFFF # shift away the x coord + 2 bit padding
+    rot = positionBlob >> 54 #shift away x, y and z coord + 3*2 padding (54 places) to get rotation to LSB
+    #print("x: %d, y: %d, z: %d | rot: %d\n" % (x,y,z,rot))
+    
+    #figure out which asset this position actually belongs to and store it 
+    subTotal = 0
+    currentAsset = 0
+    for i in range(len(assets["asset_data"])):
+        subTotal += assets["asset_data"][i]["instance_count"]
+        if (decAssetCount < subTotal): #check if the currently iterated through asset in the asset list is the one we are currently decoding the pos of
+            assets["asset_data"][i]["instances"].append({
+                "x": x,
+                "y": y,
+                "z": z,
+                "degree": rot*15
+            })
+            break
+    
+#slabFileName = input("Enter file containing slab code: ")
+slabFileName = "test_slabs/rot_prop"
+
+#read slab
+slabFile = open(slabFileName, "r")
+slabRawData = slabFile.read()
+
+#decode base64
+base64Bytes = slabRawData.encode('ascii')
+slabCompressedData = base64.b64decode(base64Bytes)
+
+#enter data into virtual gzip file, then decompress it
+gzipFile = io.BytesIO()
+gzipFile.write(slabCompressedData)
+gzipFile.seek(0)
+
+slabData = gzip.GzipFile(fileobj = gzipFile, mode = "rb").read()
+
+print("\nraw:\n", slabRawData)
+#print("\ncompressed:", slabCompressedData)
+#print("data:", slabData)
+print("\ndata:\n", ":".join("{:02x}".format(c) for c in slabData))
+
+print("\n--- parsing ---")
+header = slabData[:10]
+assets["unique_asset_count"] = unpack("I", header[6:])[0]
+uniqueAssetCount = unpack("I", header[6:])[0]
+assetList = slabData[len(header):len(header) + assets["unique_asset_count"] * assetListEntryLength]
+assetPositionList = slabData[len(header) + len(assetList):]
+
+print("- header -\n")
+print(":".join("{:02x}".format(c) for c in header))
+print("\n- asset list -\n")
+print(":".join("{:02x}".format(c) for c in assetList))
+print("\n- asset positions -\n")
+print(":".join("{:02x}".format(c) for c in assetPositionList))
+
+#decode asset list
+print("\n- decoding asset list -\n")
+print("# unique:", assets["unique_asset_count"])
+for i in range(assets["unique_asset_count"]):
+    decodeAsset(assetList[i * assetListEntryLength : (i+1) * assetListEntryLength])
+    
+#decode asset positions
+print("\n- decoding asset position list -\n")
+decAssetCount = 0
+while(len(assetPositionList[decAssetCount*assetPositionEntryLength:]) > assetPositionEntryLength):
+    decodeAssetPosition(assetPositionList[decAssetCount * assetPositionEntryLength:(decAssetCount+1) * assetPositionEntryLength], decAssetCount)
+    decAssetCount += 1
+print(assets)
+    
+    
+    
