19a6d8186487aeaa1fee2357facafcbb58edf19d
LuPro <luis.immanuel@gmx.at>
2021-09-01 06:15:16 +0200

    first iteration of slab encoder done input validation and error checking needs to be done still resolve #3

--- slabelfish.py
+++ slabelfish.py
@@ -22,6 +22,7 @@ def format_binary(data):
 
 # ------- decode part start
 
+#replace with .from_bytes?
 def assemble_bytes(bytes):
     total = 0
     for byte in bytes:
@@ -85,17 +86,13 @@ def decode(data, asset_list_entry_length, asset_position_entry_length, verbose=F
     base64_bytes = data.encode('ascii')
     slab_compressed_data = base64.b64decode(base64_bytes)
 
-    #enter data into virtual gzip file, then decompress it
-    gzip_file = io.BytesIO()
-    gzip_file.write(slab_compressed_data)
-    gzip_file.seek(0)
-
-    slab_data = gzip.GzipFile(fileobj = gzip_file, mode = "rb").read()
+    #decompress gzip
+    slab_data = gzip.decompress(slab_compressed_data)
     
     print_info("info_quiet", "Binary slab data after first base64 decoding, then gzip unpacking:\n" + format_binary(slab_data) + "\n", verbose, quiet)
 
     print_info("info_quiet", "--- Starting to parse slab", verbose, quiet)
-    print_info("info_quiet", "    Decoding slab header and splitting data into parts", verbose, quiet)
+    print_info("info_quiet", "  - Decoding slab header and splitting data into parts", verbose, quiet)
     header = slab_data[:10]
     out_json["unique_asset_count"] = unpack("I", header[6:])[0]
     asset_list = slab_data[len(header):len(header) + out_json["unique_asset_count"] * asset_list_entry_length]
@@ -125,15 +122,94 @@ def decode(data, asset_list_entry_length, asset_position_entry_length, verbose=F
 # ------- decode part end
 
 
+
+
+
 # ------- encode part start
 
+def create_header(unique_asset_count, verbose=False, quiet=False):
+    header = b'\xCE\xFA\xCE\xD1\x02\x00'
+    header += unique_asset_count.to_bytes(4, byteorder='little')
+    return header
+
+
+def encode_asset(asset_json, verbose=False, quiet=False):
+    print_info("info_quiet", "    - Encoding asset: " + json.dumps(asset_json), verbose, quiet)
+    asset_blob = b''
+    uuid_parts = asset_json['uuid'].split("-")
+    uuid_bytes = b''
+    uuid_bytes += int(uuid_parts[0], 16).to_bytes(4, byteorder='little')
+    uuid_bytes += int(uuid_parts[1], 16).to_bytes(2, byteorder='little')
+    uuid_bytes += int(uuid_parts[2], 16).to_bytes(2, byteorder='little')
+    uuid_bytes += int(uuid_parts[3], 16).to_bytes(2, byteorder='big')
+    uuid_bytes += int(uuid_parts[4], 16).to_bytes(6, byteorder='big')
+    asset_blob = uuid_bytes + asset_json['instance_count'].to_bytes(4, byteorder='little')
+    print_info("info_quiet", "      Created asset list entry blob:\n        " + format_binary(asset_blob) + "\n", verbose, quiet)
+    return asset_blob
+    
+
+#encodes an asset position
+def encode_asset_position(instance_json, verbose=False, quiet=False):
+    print_info("info_quiet", "          Encoding asset position: " + json.dumps(instance_json), verbose, quiet)
+    position_blob = 0
+    position_blob |= int(instance_json['x'])
+    position_blob |= (int(instance_json['y']) << 36)
+    position_blob |= (int(instance_json['z']) << 18)
+    position_blob |= (int(instance_json['degree'] / 15) << 54)
+    print_info("info_quiet", "          Created position blob:\n            " + format(position_blob, '064b') + "\n            " + format_binary(position_blob.to_bytes(8, byteorder='little')) + "\n", verbose, quiet)
+    
+    return position_blob.to_bytes(8, byteorder='little')
+    
+    
+# creates asset list and asset position list     
+def create_assets_data(assets_json, verbose=False, quiet=False):
+    asset_list = b''
+    position_list = b''
+    
+    for asset in assets_json: # create an entry in the asset list for each asset
+        asset_list_entry = encode_asset(asset, verbose, quiet)
+        asset_list += asset_list_entry
+        for instance in asset['instances']: # create an entry in the position list for each instance of each asset
+            print_info("info_quiet", "      - Creating position list for asset", verbose, quiet)
+            position_list_entry = encode_asset_position(instance, verbose, quiet)
+            position_list += position_list_entry
+            
+    return (asset_list, position_list)
+            
 def encode(data, verbose=False, quiet=False):
+    slab_data = b'' # byte string slab blob
+    encode_asset_position(json.loads('{"x": 5, "y": 13, "z": 611, "degree": 345}'), verbose, quiet)
+
     slab_json = json.loads(data)
+    
+    print_info("info_quiet", "--- Starting to create binary slab blob", verbose, quiet)
+    # Updating internal json fields
+    print_info("info_quiet", "    Updating unique asset count field", verbose, quiet)
     slab_json['unique_asset_count'] = len(slab_json['asset_data']) # no input validation! possibly keep track of duplicate entries (are they actually unique?) and either resolve or give warning in case of duplicates. Also no validation if # unique assets are too many for the format
+    # also doesn't add in the proper order for a nice output if field was missing entirely. not important for function, but would be nice for usability
     for asset in slab_json['asset_data']:
         asset['instance_count'] = len(asset['instances'])
-        
-    print(json.dumps(slab_json, indent=2))
+        #should do more validation on instances, but probably needs knowledge if a uuid stands for a tile or prop to differentiate between allowed positions and rotations of each of those
+    # need to find the asset with the 0,0,0 tile and put it first in the lists (is this actually needed?)
+    print_info("info_quiet", "  - Creating header", verbose, quiet)
+    slab_data += create_header(len(slab_json['asset_data']), verbose, quiet)
+    print_info("info_quiet", "      Header: " + format_binary(slab_data), verbose, quiet)
+    print_info("info_quiet", "  - Creating asset list and position list", verbose, quiet)
+    asset_data = create_assets_data(slab_json['asset_data'], verbose, quiet)
+    slab_data += asset_data[0] + asset_data[1] + b'\x00\x00'
+    
+    print_info("info_quiet", "    Binary slab data:\n" + format_binary(slab_data), verbose, quiet)
+    
+    #gzip compress
+    print_info("info_quiet", "  - Compressing binary slab data with gzip:", verbose, quiet)
+    slab_compressed_data = gzip.compress(slab_data)
+    
+    #base64 encode
+    print_info("info_quiet", "  - Base64 encoding gzip compressed data", verbose, quiet)
+    base64_bytes = base64.b64encode(slab_compressed_data)
+    
+    return b'```' + base64_bytes + b'```'
+    
     
 # ------- encode part end
 
@@ -244,9 +320,11 @@ def main():
     elif (exec_data['mode'] == 'automatic'):
         #if (re.fullmatch('^```.*```$|^{.*}$', data) != None):
         if (re.fullmatch('^```.*```$', data) != None): # Decode
+            exec_data['mode'] = 'decode'
             print_info("info", "Interpreted input as slab data, setting to decode.", exec_data['verbose'], exec_data['quiet'])
             out_data = decode(data, asset_list_entry_length, asset_position_entry_length, exec_data['verbose'], exec_data['quiet'])
         elif (re.fullmatch('^{.*}$', data, flags=re.DOTALL) != None): #Encode
+            exec_data['mode'] = 'encode'
             print_info("info", "Interpreted input as JSON data, setting to encode.", exec_data['verbose'], exec_data['quiet'])
             out_data = encode(data, exec_data['verbose'], exec_data['quiet'])
         else:
@@ -258,11 +336,17 @@ def main():
         
     if (exec_data['out'] == None):
         print_info("info_quiet", "\n---\n\nResult:", True, exec_data['quiet'])
-        #print(json.dumps(out_data, indent=2)) #make cmd argument for pretty print? default pretty with flag for short?
-        print_info("info_quiet", json.dumps(out_data), True, exec_data['quiet'])
+        if (exec_data['mode'] == 'decode'): # can I do that nicer?
+            #print(json.dumps(out_data, indent=2)) #make cmd argument for pretty print? default pretty with flag for short?
+            print_info("info_quiet", json.dumps(out_data), True, exec_data['quiet'])
+        elif (exec_data['mode'] == 'encode'):
+            print_info("info_quiet", out_data, True, exec_data['quiet'])
     else:
         out_file = open(exec_data['out'], "w")
-        out_file.write(json.dumps(out_data, indent=2))
+        if (exec_data['mode'] == 'decode'): # can I do that nicer?
+            out_file.write(json.dumps(out_data, indent=2))
+        elif (exec_data['mode'] == 'encode'):
+            out_file.write(out_data)
         out_file.close()
         
     sys.exit(0)
